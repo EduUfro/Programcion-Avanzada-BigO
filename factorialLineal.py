e3bc51f97de39f8ec855b9c821d277b416ef7c60
EduUfro <e.andrades01@ufromail.cl>
2026-08-26 13:12:18 -0400

    commit
    time

--- factorialLineal.py
+++ factorialLineal.py
@@ -1,7 +1,11 @@
+import time
 def factorial(n):
     factor = 1
     for i in range(1, n+1):
         factor = factor * i
     return factor
-print(factorial(100))
+inicio = time.time()
+print(factorial(1000))
+fin= time.time()
+print(fin-inicio)
 
--- factorialRecursivo.py
+++ factorialRecursivo.py
@@ -1,7 +1,11 @@
+import time
 def factorial(n):
 	if n==0 or n==1:
 		return 1
 	return n * factorial(n-1)
+inicio = time.time()
 print(factorial(100))
+fin = time.time()
+print(fin-inicio)
 
 	
